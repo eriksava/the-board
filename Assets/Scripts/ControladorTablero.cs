ceccbfc72fc96d5a72ddcca85e53662aaf96d246
Erik S <eriksava@hotmail.com>
2018-10-02 02:44:57 -0700

    rotation done

---
+++ Assets/Scripts/ControladorTablero.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorTablero : MonoBehaviour {
+
+
+    public float velocidad = 20;
+
+	void Update () {
+
+        ControlarTablero();
+     
+    }
+
+
+    void ControlarTablero(){
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (transform.rotation.x > 0.15)
+            {
+
+                transform.Rotate(0, 0, 0);
+
+            }
+            else
+            {
+
+                transform.Rotate(velocidad * Time.deltaTime, 0, 0);
+
+            }
+
+        }
+
+        else if (Input.GetKey(KeyCode.S))
+        {
+
+            if (transform.rotation.x < -0.15)
+            {
+
+                transform.Rotate(0, 0, 0);
+
+            }
+            else
+            {
+
+                transform.Rotate(-velocidad * Time.deltaTime, 0, 0);
+
+            }
+
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+
+            if (transform.rotation.z > 0.15)
+            {
+
+                transform.Rotate(0, 0, 0);
+
+            }
+            else
+            {
+
+                transform.Rotate(0, 0, velocidad * Time.deltaTime);
+
+            }
+
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+
+            if (transform.rotation.z < -0.15)
+            {
+
+                transform.Rotate(0, 0, 0);
+
+            }
+            else
+            {
+
+                transform.Rotate(0, 0, -velocidad * Time.deltaTime);
+
+            }
+
+        }
+
+
+    }
+}

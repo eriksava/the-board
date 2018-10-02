82b97f4e84ecb714ae8a9584399f177a67d97bb7
Erik S <eriksava@hotmail.com>
2018-10-02 10:40:04 -0700

    gameplay done

---
+++ Assets/Scripts/AlEntrarHaz.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlEntrarHaz : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(this.gameObject);
+    }
+
+
+
+}
+
+
---
+++ Assets/Scripts/Rotar.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotar : MonoBehaviour {
+
+
+    public Vector3 direccion;
+
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(direccion);
+	}
+}

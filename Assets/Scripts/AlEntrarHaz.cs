9b835253ab70a5eb5d7c137a5fdd3b11448b3285
Erik S <eriksava@hotmail.com>
2018-10-02 17:44:38 -0700

    game ready

--- Assets/Scripts/AlEntrarHaz.cs
+++ Assets/Scripts/AlEntrarHaz.cs
@@ -5,8 +5,21 @@ using UnityEngine;
 public class AlEntrarHaz : MonoBehaviour
 {
 
+    public AudioClip clip;
+
+    float volumen = 1.0f;
+
+    Transform posicion;
+
+
+    private void Start()
+    {
+        posicion = transform;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
+        AudioSource.PlayClipAtPoint(clip, posicion.position, volumen);
         Destroy(this.gameObject);
     }
 
